7cd8a8b256f4ec06badc3dde0cfcec08dc7ef5cc
Libing Chen <libing.chen@gmail.com>
2024-11-21 10:54:24 +0800

    chore: add generate_headers

--- src/main/rust/double-number/src/lib.rs
+++ src/main/rust/double-number/src/lib.rs
@@ -1,5 +1,15 @@
-#[no_mangle]
+use safer_ffi::prelude::*;
+
 #[allow(non_snake_case)]
-pub extern "C" fn doubleNumber(n: i32) -> i32 {
+#[ffi_export]
+fn doubleNumber(n: i32) -> i32 {
     n * 2
+}
+
+#[safer_ffi::cfg_headers]
+#[test]
+fn generate_headers() -> ::std::io::Result<()> {
+    ::safer_ffi::headers::builder()
+        .to_file("bindings.h")?
+        .generate()
 }
